2956830cd7db4b1664fca3da15b7ecafaf40460b
David Bodine <davidhbodine@gmail.com>
2023-08-10 10:17:16 -0600

    Final Product

--- pages/Weight_Loss_Predictors.py
+++ pages/Weight_Loss_Predictors.py
@@ -1,9 +1,4 @@
 import streamlit as st;
-import pandas as pd;
-import numpy as np;
-import seaborn as sns;
-import matplotlib.pyplot as plt 
-import statsmodels.api as sm
 
 stepcount_coefficent = 0.0826
 st.title('Weight Loss Predictor:')
