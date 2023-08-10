cb3eab44fe10476b4ddbfebd617f893686c5ed79
David Bodine <davidhbodine@gmail.com>
2023-08-10 07:22:07 -0600

    Finished Findings

---
+++ pages/Weight_Loss_Predictors.py
@@ -0,0 +1,8 @@
+import streamlit as st;
+import pandas as pd;
+import numpy as np;
+import seaborn as sns;
+import matplotlib.pyplot as plt 
+import statsmodels.api as sm
+
+st.title('Weight Loss Predictor:')
